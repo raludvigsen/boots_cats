3fb31ee1e8ad5061b6b07b41662be4618d361679
Robert Ludvigsen <robertludvigsen@Roberts-MacBook-Pro.local>
2015-12-11 12:59:24 -0500

    adds matrix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,10 @@ console.log("working");
 $( document ).ready(function() {
 
 	nx.onload = function() {
-		matrix1.row = 4;
-		matrix1.col = 16;
-		matrix1.colors.accent = "#7BD389"
-		matrix1.init();
+		drumkit.row = 4;
+		drumkit.col = 16;
+		drumkit.colors.accent = "#7BD389"
+		drumkit.init();
 	}
 
 })
